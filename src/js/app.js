58953e37a085f88c41e28e4e550950c45a43ac09
Михаил Вахтуров <vahturov.m@fredtm.ru>
2022-06-22 06:51:58 +0000

    Master

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,8 @@ flsFunctions.isWebp();
 import "./modules/main-menu.js";
 import "./modules/faq.js";
 import "./modules/sliders.js";
+import "./modules/tabs.js";
+import "./modules/department-slider.js";
+import "./modules/department-cardTabs.js";
+import "./modules/department-cardHeading.js";
 
