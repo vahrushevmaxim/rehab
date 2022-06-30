165e727ca8cbcfd4c8c93bd842a8203610eba80a
Maxim <vahrushev@fredtm.ru>
2022-06-30 16:01:59 +0400

    commit

--- src/js/modules/faq.js
+++ src/js/modules/faq.js
@@ -5,10 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
         items.forEach(function(item) {
             const btn = item.querySelector('.faq__btn-wrap');
             btn.addEventListener('click', function() {
-                console.log(item.querySelector('.faq__item-content'));
+                ///console.log(item.querySelector('.faq__item-content'));///
                 item.querySelector('.faq__item-content').classList.toggle('faq__active');
                 btn.classList.toggle('faq__btn-act');
             });
         })
     }
 })
+
+
+
+
