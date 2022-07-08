679ac3a9e612d558c29c08f630e5803f4af6ae82
Maxim <vahrushev@fredtm.ru>
2022-07-08 15:14:00 +0400

    commit

--- src/js/modules/contacts-modal.js
+++ src/js/modules/contacts-modal.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let modal 
+    let close = document.querySelector(".contacts__modal-close");
+    let modalWindow = document.querySelector(".contacts__modal-window");
+    let button = document.querySelector(".contacts__btn");
+    let body = document.querySelector("body");
 
+    button.addEventListener("click", function () {
+        modalWindow.classList.toggle("active");
+        body.style.overflowY = 'hidden';
+    });
 
-
+    close.addEventListener("click", function () {
+        modalWindow.classList.remove("active");
+        body.style.overflowY = 'visible';
+    });
 
 });
--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,12 +1,50 @@
-let myMap;
+document.addEventListener('DOMContentLoaded', function () {
+    let myMap;
+    function init() {
+        myMap = new ymaps.Map('map-1', {
+            center: [56.136649716098695, 37.57158493767343],
+            zoom: 16
+        })
+        let myGeoObject = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: [56.13661638949428,37.571456303448]
+            },
+            properties: {
+                iconContent: 'Реабилитационный центр',
+            }
+        })
+    }
+    ymaps.ready(init)
+    
+    
 
-function init() {
-    myMap = new ymaps.Map('map-1', {
-        center: [],
-        zoom: 10
-    },
+    let myMap2;
+    function init2() {
+        myMap2 = new ymaps.Map('map-2', {
+            center: [55.753377068970586,37.64053350000001],
+            zoom: 16
+        })
+        let myGeoObject2 = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: [55.753377068970586,37.64053350000001]
+            },
+            properties: {
+                iconContent: 'Амбулаторный центр',
+            }
+        })
+    }
+    ymaps.ready(init2)
 
+    let myMap3;
+    function init3() {
+        myMap3 = new ymaps.Map('map-3', {
+            center: [55.777288050559015,37.613395189609115],
+            zoom: 11
+        })
+    }
+    ymaps.ready(init3)
 
-    )
-}
+})
 
