fb227f9ba62e24f9454c22b5b190e8e0eb4613f0
Maxim <vahrushev@fredtm.ru>
2022-08-26 12:49:03 +0400

    commit

--- src/js/modules/about-video-play.js
+++ src/js/modules/about-video-play.js
@@ -1,16 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     if (document.querySelector('.about__video')) {
-        const video = document.querySelector('.about__video source');
+        const video = document.querySelector('#about-video');
         const playBtn = document.querySelector('.about__preview-wrapper');
 
         function toggleVideo() {
-            if (video.paused) {
-                video.play();
-                playBtn.style.dispay = 'block';
-            } else {
-                video.pause();
-                playBtn.style.dispay = 'none';
-            }
+            playBtn.style.display = 'none';
+            video.play();
         }
 
         playBtn.addEventListener('click', toggleVideo);
